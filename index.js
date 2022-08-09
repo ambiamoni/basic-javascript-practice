81b07fd4ffad357cb0e8d55667c9be1ad3178656
ambiamoni <ambiadc65@gmail.com>
2022-08-09 09:01:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// #prob:1
+var name = "ambia";
+isSheAWoman = true;
+var age = 42;
+
+// #prob:2
+const myName = "ambia";
+let myClass = "seven";
+myClass = "eight";
+
+// #prob:3
+const a = 5;
+const b = 9;
+const sum = a + b;
+const multiply = a * b;
+const divided = a / b;
+const modulas = a % b;
+console.log(sum);
+
+
+
+
+
