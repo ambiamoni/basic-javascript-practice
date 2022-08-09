3b9014628733f26ca3c4cb0941927e5aa70a0213
ambiamoni <ambiadc65@gmail.com>
2022-08-09 09:53:11 +0530

    add

--- index.js
+++ index.js
@@ -16,11 +16,44 @@ const subtraction = a - b;
 const multiply = a * b;
 const divided = a / b;
 const remainder = a % b;
-console.log(sum);
-console.log(subtraction);
-console.log(multiply);
-console.log(divided);
-console.log(remainder);
+// console.log(sum);
+// console.log(subtraction);
+// console.log(multiply);
+// console.log(divided);
+// console.log(remainder);
+
+
+// #prob: 4
+// let num1 = 8;
+// let num2 = 4;
+// if(num1>num2){
+//     console.log('num1 is big');
+// } else if (num1 < num2) {
+//     console.log('ni=um1 is small');
+// } else if (num1 == num2) {
+//     console.log('num1 and num2 both are equal');
+// } else if (num1 != num2) {
+//     console.log('num1 is not equal num2')
+// } else if (num1 >= num2){
+//     console.log('chocolate den');
+// } else{
+//     console.log('kichu khabona');
+// }
+
+// #prob5:
+
+// let money = 150;
+// let iccha = false;
+// if (money >= 200 && iccha==true) {
+//     console.log('you can buy pizza')
+// }else if(money >= 155 || iccha==true) {
+//     console.log('you also can buy pizza')
+// } else {
+//     console.log('you cannot buy pizza')
+// }
+
+
+
 
 
 
